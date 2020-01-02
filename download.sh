9e3bfffb9093024c1ec36f579aadae00856d5c07
zengxianyu <zengyu@mail.dlut.edu.cn>
2020-01-02 17:09:21 +0800

    download

---
+++ download.sh
@@ -0,0 +1,10 @@
+#wget -P data/datasets/saliency_Dataset/ http://ok.i68h.cn:8000/test.zip
+#unzip data/datasets/saliency_Dataset/test.zip -d data/datasets/saliency_Dataset/
+wget -P data/datasets/saliency_Dataset/ http://ok.biu886.com:8000/ECSSD.zip
+unzip data/datasets/saliency_Dataset/ECSSD.zip -d data/datasets/saliency_Dataset/
+
+wget -P data/datasets/saliency_Dataset/ http://ok.biu886.com:8000/DUT-train.zip
+unzip data/datasets/saliency_Dataset/DUT-train.zip data/datasets/saliency_Dataset/
+
+wget -P data/datasets/segmentation_Dataset/VOCdevkit/ http://ok.biu886.com:8000/VOC2012.zip
+unzip data/datasets/segmentation_Dataset/VOCdevkit/VOC2012.zip -d data/datasets/segmentation_Dataset/VOCdevkit/
